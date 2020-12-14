5737a42df4a7f13388546787a52d4f18f84aa5c1
AM <michael.rybintsev@gmail.com>
2020-12-14 17:42:21 +0000

    adds csharp hello-world examples

---
+++ LocksAndThreads/csharp/csharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld.RunExample();
+        }
+        
+        private static class HelloWorld
+        {
+            public static void RunExample()
+            {
+                var myThread = new Thread(ThreadWork);
+                myThread.Start();
+                Thread.Yield(); // this somewhat improves chances of new thread finishing before the main one. 
+                // just as Paul Butcher mentions in the book, without yielding, main thread mostly finishes first. 
+                Console.WriteLine("Hello from main thread");
+            }
+            
+            private static void ThreadWork()
+            {
+                Console.WriteLine("Hello from new thread");
+            }
+        }
+    }
+}
