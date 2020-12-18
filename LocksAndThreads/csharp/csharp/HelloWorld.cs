717721fc36a2327377f24ff322b4b13784e5d37e
AM <michael.rybintsev@gmail.com>
2020-12-18 14:22:54 +0000

    refactor c# examples

---
+++ LocksAndThreads/csharp/csharp/HelloWorld.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Threading;
+
+namespace csharp
+{
+    public static class HelloWorld
+    {
+        public static void RunExample()
+        {
+            var myThread = new Thread(() => Console.WriteLine("Hello from new thread"));
+            myThread.Start();
+            Thread.Yield(); // this somewhat improves chances of new thread finishing before the main one. 
+            // just as Paul Butcher mentions in the book, without yielding, main thread mostly finishes first. 
+            Console.WriteLine("Hello from main thread");
+        }
+    }
+}
